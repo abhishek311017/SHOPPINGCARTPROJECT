938ce85c2b329314dbd3c27f7660e169157abb1c
Abhishek <abhisheksingh31102017@gmail.com>
2022-04-11 11:56:58 +0530

    Add project files.

---
+++ SHOPPINGCARTPROJECT/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SHOPPINGCARTPROJECT.Models;
+
+namespace SHOPPINGCARTPROJECT.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Users model) 
+        {
+            var context = new ShoppingCartDbEntities();
+            
+                bool valid=context.User.Any(x=>x.EmailId==model.EmailId&&x.Password==model.Password);
+                if (valid)
+                {
+                    var x=context.User.Single(m => m.EmailId == model.EmailId);
+                    Session["Id"] = x.Id.ToString();
+                    FormsAuthentication.SetAuthCookie(model.EmailId, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid EmailId Or password");
+                return View();
+            
+                
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(SignUp model)
+        {
+
+            using (var context=new ShoppingCartDbEntities())
+            {
+                User user = new User
+                {
+                    FullName = model.FullName,
+                    MobileNumber = model.MobileNumber,
+                    RoleId = 1,
+                    EmailId = model.EmailId,
+                    Gender = model.Gender,
+                    Password = model.Password
+                };
+                //user.Id = model.Id;
+                context.User.Add(user);
+
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login", "Account");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ SHOPPINGCARTPROJECT/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using SHOPPINGCARTPROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHOPPINGCARTPROJECT.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+
+
+
+        private readonly ShoppingCartDbEntities obj;
+
+        public ProductController()
+        {
+            obj = new ShoppingCartDbEntities();
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            Products product = new Products
+            {
+                SelectProducts = from p in obj.Categories
+                                 select new SelectListItem()
+                                 {
+                                     Text = p.CategoryName,
+                                     Value = p.CategoryId.ToString(),
+                                     Selected = true
+
+                                 }
+            };
+            return View(product);
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpPost]
+        public  JsonResult Index(Products productmodel)
+        {
+            string Newimg = Guid.NewGuid() + Path.GetExtension(productmodel.Image.FileName);
+            productmodel.Image.SaveAs(Server.MapPath("~/Images/" + Newimg));
+            Product ob = new Product
+            {
+                Image = "~/Images/" + Newimg,
+                CategoryId = productmodel.CategoryId,
+                ProductName = productmodel.ProductName,
+                Price = productmodel.Price,
+                Description = productmodel.Description,
+                ProductId = Guid.NewGuid()
+            };
+            obj.Product.Add(ob);
+            obj.SaveChanges();
+            return Json("true",JsonRequestBehavior.AllowGet);
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+
+                var pro = obj.Product.SingleOrDefault(x => x.ProductId.ToString().ToLower() == id);           
+                obj.Product.Remove(pro);
+                obj.SaveChanges();
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Index", "Home", new { id, saveChangesError = true });
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
